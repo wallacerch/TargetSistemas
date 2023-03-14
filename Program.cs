3f2a1dbe033fad76970794c78417bdc5f6665081
Wallace Rocha <wr.wallacerocha@gmail.com>
2023-03-14 20:29:55 -0300

    TargetSistemas

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //SEGUNDA QUESTÃO
+        Console.Write("Digite um número inteiro: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int a = 0, b = 1, c = 1;
+        while (c < numero)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        if (c == numero)
+        {
+            Console.WriteLine($"{numero} pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} não pertence à sequência de Fibonacci.");
+        }
+
+        //QUINTA QUESTÃO
+        Console.Write("Digite uma palavra: ");
+        string palavra = Console.ReadLine();
+
+        char[] charArray = palavra.ToCharArray();
+        Array.Reverse(charArray);
+
+        string palavraInvertida = new string(charArray);
+        Console.WriteLine($"A string invertida é: {palavraInvertida}.");
+    }
+}
